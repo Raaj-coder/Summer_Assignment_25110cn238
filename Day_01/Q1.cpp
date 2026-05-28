a8a7063693cdb1f6f9628fceaee5fa9dfe71c576
Raj verma <js5321612@gmail.com>
2026-05-28 20:21:55 +0530

    Day 1

--- Day_01/Q1.cpp
+++ Day_01/Q1.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-using namespace std;; 
-int main(){
-     cout<<"hey raaj !" <<endl ;
-      cout<< "raaj hu bhai ";
+using namespace std;
+int main() {
+    int n, i, sum = 0;
+    cout << "Enter the number: ";
+    cin >> n;
+    for(i = 0; i <= n; i++) {
+        sum = sum + i;
+    }
+    cout << "The sum is " << sum;
     return 0;
 }
