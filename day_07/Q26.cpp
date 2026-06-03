2820c162a59b61e27c700dad3597d709d2389570
Raj verma <js5321612@gmail.com>
2026-06-03 12:59:42 +0530

    day_07

---
+++ day_07/Q26.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int fib(int n)
+{
+    if(n == 0 || n == 1)
+        return n;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "Fibonacci term = " << fib(n);
+    return 0;
+}
